3e0dffc2fc4bf8e4c2977784222c9da8a9b55617
ThiagoBezerraa <doogGCThiago@gmail.com>
2022-10-12 19:04:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
+client = commands.Bot(command_prefix='>', intents=intents)
+
 @bot.command()
 async def feidms(ctx):
     await ctx.send('vc')
@@ -14,8 +16,17 @@ async def kirufeio(ctx):
     await ctx.send('concordo')
 
 @bot.command()
-async def embed(ctx):
-    embed=discord.Embed(title="ALEK GAYZAO", url="https://upload.wikimedia.org/wikipedia/en/8/87/Keyboard_cat.jpg", description="GATO FODA TOCANDO PIANO", color=0xFF5733)
-    await ctx.send(embed=embed)
+async def cat(ctx):
+    embed=discord.Embed(
+     title="ALEK GAYZAO",
+     url="https://www.youtube.com/watch?v=J---aiyznGQ&ab_channel=KeyboardCat2020",
+     description="GATO FODA TOCANDO PIANO",
+     color=0xFF5733)
 
-bot.run('MTAyOTgxOTc0NjkxODYwMDc4NA.Gj90FW.-PGGJ-WiOIKMLyBkwSOAIaFkuBVTb1BPFAdVHA')
+    embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/en/8/87/Keyboard_cat.jpg')
+    embed.add_field(name='Kauantibo', value='viado com certeza', inline=True
+    )
+
+    await ctx.send(embed = embed)
+
+bot.run('')
